05db2025cd6a9e19b8269721a27ee50a04aedfbc
RayWallacePhotos <114260570+RayWallacePhotos@users.noreply.github.com>
2023-01-04 22:38:25 -0500

    Major changes

--- javascript/html.js
+++ javascript/html.js
@@ -13,27 +13,29 @@ let VersionHtmlJs = "1.0";
 //
 // Called by init() in main.js
 //
-function htmlInit( page ) {
+function htmlInit( page) {
+  let menuElement = document.querySelector( "menu" );
   let titleElement = document.querySelector( ".Title" );
   let htmlsElement = document.querySelector( ".Htmls" );
 
-  page[0] = page[0].toUpperCase();
-  titleElement.innerHTML = `<h1>${page.toUpperCase()}</h1>`
+  titleElement.innerText = capitalize( page );
 
   fileFileOfTextFiles( page+".txt", {basePath: `../${page}`,
     callback: data => {
       if( data.text ) {
-        let name = data.fileName;
-        let slashIndex = name.lastIndexOf('/');
-        if( slashIndex == -1 ) slashIndex = name.lastIndexOf('\\');
-        if( slashIndex != -1 ) name = name.slice( slashIndex + 1 );
-
+        let id;
+        let name = filename( data.fileName );
         // We now have filename with NO dir path, so remove the extension
         name = name.slice( 0, name.lastIndexOf('.') );
 
+        id = makeDOMId( name );
+
+//        menuElement.innerHTML += `<li>${name.split("_").join(" ")}</li>`;
+        menuElement.innerHTML += `<li><a href="#${id}">${name.split("_").join(" ")}</a></li>`;
+
         htmlsElement.innerHTML += `<section class="Html BoxShadow">
           <!-- <h1>${name}</h1> -->
-          ${data.text}
+          <div id="${id}"> ${data.text}</div>
         </section>`;
       }
     }
--- javascript/images.js
+++ javascript/images.js
@@ -14,32 +14,25 @@ let VersionImagesJs = "1.0";
 // Called by init() in main.js
 //
 function imagesInit( page ) {
+  let basePath = `../images/${page}/`;
   let imagesElement = document.querySelector( ".Images" );
 
-  imagesElement.innerHTML = `<h1>${page.toUpperCase()}</h1>`
+  document.querySelector( ".Title" ).innerText = capitalize( page );
 
-  fileFileOfImageFiles( page+".txt", {basePath: `../images/${page}`,
-    callback: data => {
-      if( data.name ) {
-        let name = data.name;
-        let slashIndex = name.lastIndexOf('/');
-        if( slashIndex == -1 ) slashIndex = name.lastIndexOf('\\');
-        if( slashIndex != -1 ) name = name.slice( slashIndex + 1 );
-
-        // We now have filename with NO dir path, so remove the extension
-        name = name.slice( 0, name.lastIndexOf('.') );
-
-        imagesElement.innerHTML += `
-          <span class="Container">
-            <img loading="lazy" src="${data.name}"img>
-            <!-- <h2>${name}</h2> -->
+  fileReadText( page+".txt",
+    data => {
+      if( data.text ) {
+        for( let fileName of data.text.trim().split("\n") ) {
+          imagesElement.innerHTML += `
+          <span" class="Container">
+            <img src=${basePath+fileName}>
           </span>
-        `;
+          `;
+        }
       }
-    }
   } );
-}
 
+}
 
 
 
